b0089628530e2388e4ff56ec0c450d8dc84c220f
j-loucel <j15loucel@gmail.com>
2021-12-22 11:43:02 +0100

    Random function to select message

---
+++ MixedMessages.js
@@ -0,0 +1,4 @@
+const helloMessages = ["Hello,", "Hey,", "Hi,", "Hello there,", "Yo!", "How you doin'?", "Nice to see you!"];
+const superHeros = ["Batman", "Superman", "Flash", "Hulk", "Spiderman", "Ironman", "Doctor Strange", "Wonder Woman", "Black Widow", "Scarlet Witch", "Captain Marvel"];
+const wishMessage = ["have a nice day!", "I hope you doing OK.", "good luck.", "keep pushing mate.", "don't stop now.", "keep working hard."];
+
