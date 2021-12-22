6e8b4aaa07e7ae9a6114508359a3b4bb754031d5
j-loucel <j15loucel@gmail.com>
2021-12-22 11:44:45 +0100

    Random function to select message

--- MixedMessages.js
+++ MixedMessages.js
@@ -2,3 +2,13 @@ const helloMessages = ["Hello,", "Hey,", "Hi,", "Hello there,", "Yo!", "How you 
 const superHeros = ["Batman", "Superman", "Flash", "Hulk", "Spiderman", "Ironman", "Doctor Strange", "Wonder Woman", "Black Widow", "Scarlet Witch", "Captain Marvel"];
 const wishMessage = ["have a nice day!", "I hope you doing OK.", "good luck.", "keep pushing mate.", "don't stop now.", "keep working hard."];
 
+const selectMessage = (message) => {
+    const randomIndex = Math.floor(Math.random() * message.length);
+    return message[randomIndex];
+}
+
+const greeting = selectMessage(helloMessages);
+const character = selectMessage(superHeros);
+const wishes = selectMessage(wishMessage);
+
+console.log(`${greeting} I'm ${character}, ${wishes}`);
